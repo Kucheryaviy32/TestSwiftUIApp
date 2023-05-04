84c460bc2d4667ff77401015eecfc762e4371aac
Игорь Скворцов <skvortsov_iv_siv@mail.ru>
2023-05-04 17:42:44 +0300

    Initial Commit

---
+++ TestSUI/TestSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSUIApp.swift
+//  TestSUI
+//
+//  Created by Игорь Скворцов on 04.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct TestSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
